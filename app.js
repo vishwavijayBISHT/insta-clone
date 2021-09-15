5a6c669203032f2cdb2a895334e35f698453f2b4
VishwaVijay <vishwavijay.bisht@techchefz.com>
2021-09-15 23:05:52 +0530

    mongo

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const { MONGO_URI } = require("./keys");
 const PORT = 5000;
 
-// app.use() this is how to use middle ware
+// app.use() this is how to use middle ware to make all the routes use it
+
+// ot be specfic app.get("/", middleware,(req, res) => {
+//   res.send("yo");
+// });
+
+mongoose.connect(MONGO_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("database");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("database error", err);
+});
 
 app.get("/", (req, res) => {
   res.send("yo");
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://setu:Ik5RYZFPlgsOgwYv@cluster0.j6mor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
