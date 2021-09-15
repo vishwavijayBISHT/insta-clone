a698ad0d2615e32895f17fbce396fb49db6d7db7
VishwaVijay <vishwavijay.bisht@techchefz.com>
2021-09-15 22:45:22 +0530

    new node

--- app.js
+++ app.js
@@ -1,52 +1,13 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000;
-const { MONGOURI } = require("./config/keys");
+const PORT = 5000;
 
-mongoose.connect(MONGOURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-mongoose.connection.on("connected", () => {
-  console.log("connect to mongo ");
-});
-mongoose.connection.on("error", () => {
-  console.log("opps mongo error ");
+// app.use() this is how to use middle ware
+
+app.get("/", (req, res) => {
+  res.send("yo");
 });
 
-require("./models/user");
-require("./models/post");
-
-app.use(express.json());
-app.use(require("./routes/auth"));
-app.use(require("./routes/post"));
-app.use(require("./routes/user"));
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  console.log("in preduction");
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    console.log("in preduction");
-  });
-}
 app.listen(PORT, () => {
-  console.log("Listing on 5000");
+  console.log("server is running");
 });
-
-// const customMiddleware=(req,res,next)=>{
-//     console.log("middleware")
-//     next()
-// }//which takes the incoming req and modifies it before it reaches to route handler
-// // app.use(customMiddleware);//for all the routes
-// app.get('/',(req,res)=>{
-//     console.log('home')
-//   res.send("heloo world")
-// })
-// app.get('/about',customMiddleware,(req,res)=>{
-//     console.log('about')
-//   res.send("habout page")
-// })
--- models/re3d.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const re = new mongoose.Schema({});
-
-mongoose.model("RE", re);
